e450f58fdd960b9f023d053da10cfb5abd0699c9
Firede <firede@firede.com>
2026-06-20 23:40:23 +0800

    docs: 新增工作语言立论

--- www/src/data/arguments.ts
+++ www/src/data/arguments.ts
@@ -2,6 +2,7 @@ export const argumentIds = [
   'argument/what-is-project-truth',
   'argument/project-truth-in-codebase',
   'argument/project-truth-by-domain',
+  'argument/project-natural-language-in-work-language',
 ] as const;
 
 export type ArgumentId = (typeof argumentIds)[number];
