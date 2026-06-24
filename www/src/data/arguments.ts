64ddf2c8b09ea84bcbae81dba4f8c0abe66d5e53
Firede <firede@firede.com>
2026-06-24 17:11:58 +0800

    docs: 调整工作语言立论文件名

--- www/src/data/arguments.ts
+++ www/src/data/arguments.ts
@@ -2,7 +2,7 @@ export const argumentIds = [
   'argument/what-is-project-truth',
   'argument/reference-basis-versioned-with-execution-part',
   'argument/project-truth-by-domain',
-  'argument/project-natural-language-in-work-language',
+  'argument/reference-basis-work-language',
 ] as const;
 
 export type ArgumentId = (typeof argumentIds)[number];
